f5272e5e2ff9a37e579c3432ce85381a19c1fc1b
jamesstidard <jamesstidard@gmail.com>
2016-07-25 19:43:11 +0100

    dev deploy

--- app/consts_dist.js
+++ app/consts_dist.js
@@ -1,3 +1,3 @@
 export const debug = false
-export const control_url = 'https://a2z-apptitude.herokuapp.com/control.js'
+export const control_url = 'https://a2z-apptitude-dev.herokuapp.com/control.js'
 export const hash_routing = false
